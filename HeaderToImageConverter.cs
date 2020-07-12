b15dacf34119587b554fcb01361ab7b00c623c4b
Mariusz Wieczorek <m.wieczorek1972@gmail.com>
2020-07-12 12:35:02 +0200

    Add project files.

---
+++ HeaderToImageConverter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+/* Konwertery wartości są bardzo często używane przy wiązaniu danych. Oto kilka podstawowych przykładów:
+ * 
+ * Chciałbyś zaznaczyć CheckBox na podstawie wartości zmiennej, ale ta wartość jest tekstem np. "tak" lub "nie", a nie wartością typu Boolean (true/false).
+ * Masz wartość liczbową, ale chciałbyś pokazać wartość zero w jednych przypadkach, a liczby dodatnie w pozostałych
+ * Masz wielkość pliku wyrażoną w bajtach, ale chciałbyś ją pokazać w bajtach, kilobajtach, megabajtach lub gigabajtach, w zależności od jej wielkości
+ * Na przykład chcesz ustawić zaznaczenie kontrolki CheckBox na podstawie zmiennej typu Boolean, 
+ * ale na odwrót - to znaczy chciałbyś, aby CheckBox był zaznaczony, gdy zmienna jest równa "false",
+ * a niezaznaczony, gdy zmienna jest równa "true".
+ * Mógłbyś nawet wykorzystać konwerter do wygenerowania obrazka i ustawienia go w ImageSource w zależności od wartości zmiennej.
+ * Na przykład sygnał zielony dla wartości "true" i czerwony dla "false". Możliwości są praktycznie nieograniczone!
+ * 
+ * Te niewielkie klasy, które implementują interfejs IValueConverter, będą działać jak pośrednicy i tłumaczyć wartości między źródłem a miejscem docelowym.
+ * We jakichkolwiek przypadkach, gdy potrzebujesz dokonać transformacji danych w jedną lub w drugą stronę - najprawdopodobniej potrzebujesz konwertera.
+ * 
+ * Jak wspomniano wcześniej - konwerter wartości WPF wymaga zaimplementowania interfejsu IValueConverter lub alternatywnie interfejsu IMultiValueConverter
+ * Oba interfejsy wymagają tylko zaimplementowania dwóch metod: Convert() i ConvertBack().
+ * Jak sama nazwa wskazuje, te metody będą używane do konwersji wartości do docelowego formatu oraz w drugą stronę - do formatu źródłowego.
+ * 
+ */
+
+namespace WpfApp02_TreeView
+{
+    /// <summary>
+    /// Converts a full path to a specific image type of drive, folder or file
+    /// </summary>
+    [ValueConversion(typeof(string),typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        // konwertujemy string to image
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // możemy w tym miejscu zwrócić bezpośrednio ścieżkę do obrazka, tak jak byśmy wpisali ją w kodzie XAML
+            // return "Images/drive.png";
+
+
+            var path = (string)value;
+            if (path == null)
+                return null;
+
+            
+            // domyślnie zakładamy, że węzeł w drzewie katalogów jest plikiem
+            var image = "Images/file.png";
+
+            // za pomocą metody GetFileFolderName wyodrębniamy ze ścieżki nazwę pliku lub katalogu
+            // jeżeli jest ona pusta to zakłądamy, że mamy do czynienia z napędem
+            // if the name is blank we presume it's drive as we cannot have a blank file or a folder name
+            // dalej po przez sprawdzenie atrybutu pliku sprawdzamy czy jest to katalog
+            var name = MainWindow.GetFileFolderName(path);
+                        
+            
+            if (string.IsNullOrEmpty(name))
+                image = "Images/drive.png";
+            else if(new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory) )
+                image = "Images/folder-closed.png";
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}")); 
+            
+        }
+
+        // odwrotnie z image do string, w tym przypadku nie korzystamy z niej, więc zostawiamy domyślną implementację
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WpfApp02_TreeView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// https://docs.microsoft.com/pl-pl/dotnet/desktop-wpf/data/data-binding-overview
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region On Loaded
+
+        /// <summary>
+        /// When the application first opens
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            /// get every logical drive on the machine 
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                // create a new item for it
+                // tworzymy nowy element dla każdego dysku logicznego
+                var item = new TreeViewItem()
+                {
+                 // set the header
+                 Header = drive,
+
+                // and path
+                Tag = drive
+                };
+
+                
+               // add a dummy item 
+               // dodajemy sztuczny element o wartości null aby element dał się rozwinąć
+               item.Items.Add(null);
+
+                // Listen out for item being expanded
+                // odczytujemy foldery i pliki
+                item.Expanded += Folder_Expanded;
+
+                // Add it to the main tree view
+                // Dodanie pozycji do TreeView, 
+                // jedyne powiązanie z obiektem utworzonym w XAML po x:Name 
+                FolderView.Items.Add(item);
+            }
+        }
+       
+        #endregion
+
+        #region Folder Expandend
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+
+            #region Initial Check
+
+            
+            var item = (TreeViewItem)sender;
+
+           /// if the item only containst the dummy data 
+           if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+
+            // clear dummy data
+               item.Items.Clear();
+
+            // get the fullPath
+            var fullPath = (string)item.Tag;
+            #endregion
+
+            #region Get Folders - pobieramy listę folderów
+            // Create the Blank List for directories
+            var directories = new List<string>();
+
+            try
+            {
+                // try to get directories from the folder
+                // ignoring any issues doing so
+                var dirs = Directory.GetDirectories(fullPath);
+
+                if(dirs.Length > 0)
+                {
+                    directories.AddRange(dirs);
+                }
+
+            }
+            catch (Exception)
+            {
+                          
+            }
+
+            // for each directory ...
+            directories.ForEach(directoryPath =>
+            {
+                // Create Directory item
+                var subItem = new TreeViewItem()
+                {
+                    // set header as folder name    
+                    // Header = Path.GetDirectoryName(directoryPath),
+                    Header = GetFileFolderName(directoryPath),
+                    // set TAG as full path
+                    Tag = directoryPath
+
+                };
+
+                // Add dummy item so we can expand folder
+                subItem.Items.Add(null);
+
+                // Handling expanding
+                subItem.Expanded += Folder_Expanded;
+
+                // Add this Item do the parent
+                item.Items.Add(subItem);
+
+            });
+            #endregion
+
+            #region Get Files - pobieramy listę plików
+
+            // Create the Blank List for files
+            var files = new List<string>();
+
+            try
+            {
+                // try to get files from the folder
+                // ignoring any issues doing so
+                var fs = Directory.GetFiles(fullPath);
+
+                if (fs.Length > 0)
+                {
+                    files.AddRange(fs);
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            // for each file ...
+            files.ForEach(filePath =>
+            {
+                // Create file item
+                var subItem = new TreeViewItem()
+                {
+                    // set header as folder name    
+                    // Header = Path.GetDirectoryName(directoryPath),
+                    Header = GetFileFolderName(filePath),
+                    // set TAG as full path
+                    Tag = filePath
+
+                };
+
+                // Add this Item do the parent
+                item.Items.Add(subItem);
+
+            });
+
+            #endregion
+
+        }
+        #endregion
+
+
+        #region Helpers
+        /// <summary>
+        /// Find the File Folder name from a full path
+        /// Wyodrębniamy nazwę pliku lub katalogu z pełnej ścieżki
+        /// </summary>
+        /// <param name="path"> The full Path</param>
+        /// <returns></returns>
+        public static string GetFileFolderName(string path)
+        {
+            
+            // if we have no path return empty
+            // gdy pusta ścieżka zwracamy pusty string
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+
+            // Make all slashes -> back slashes
+            // zamieniamy wszystkie slashe na backslashe
+            // \ jest znakiem ucieczki dlatego musimy wyszukać \\
+            // pierwszy znak 
+            var normalizedPath = path.Replace('/','\\');
+
+            var lastIndex = normalizedPath.LastIndexOf('\\');
+
+            // jeżeli nie znajdujemy backslash'a zwracamy pierwotną ścieżkę
+            if (lastIndex == 0)
+                return path;     
+            
+            return path.Substring(lastIndex+1);
+                       
+        }
+        #endregion
+
+    }
+}
